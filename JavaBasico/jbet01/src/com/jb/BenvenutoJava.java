045891522768c1ec4ef4d3dc7d61ec0ad8bcdba0
Andreina <andreinaprati@gmail.com>
2022-07-08 20:00:14 -0300

    Java Basico Ejercicio: jbet01 Tema01: Conceptos de Java Basico

---
+++ JavaBasico/jbet01/src/com/jb/BenvenutoJava.java
@@ -0,0 +1,67 @@
+// @andreinaprati
+// jbet0101
+
+/* 1. Crear un 'proyecto' Java desde cero (nombrePro: jbet01)*/
+/* 2. Dentro del proyecto crear un 'paquete' (nombrePaq: com.jb) */
+/* 3. Dentro del paquete crear una 'clase' (nombreCla: BenvenutoJava) */
+/* 4. Dentro de la clase crear un 'método main' (ln14)
+/*    4.1. Imprimir todos los datos que se vieron en las sesiones */
+
+package com.jb;
+
+public class BenvenutoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Benvenuto Java");
+
+        byte variable01 = 3;
+        System.out.println(variable01);
+
+        short variable02 = 9;
+        System.out.println(variable02);
+
+        int variable03 = 21;
+        System.out.println(variable03);
+
+        long variable04 = 121;
+        System.out.println(variable04);
+
+        float variable05 = 3.3f;
+        System.out.println(variable05);
+
+        double variable06 = 11.1d;
+        System.out.print(variable06);
+
+        boolean variable07 = false;
+        System.out.println(variable07);
+
+        boolean variable08 = true;
+        System.out.println(variable08);
+
+        char variable09 = 'a';
+        System.out.println(variable09);
+
+        String variable10 = "Andreina Prati";
+        System.out.println(variable10);
+
+    }
+    // tipo de dato 'entero'
+    byte variable01 = 3;
+    short variable02 = 9;
+    int variable03 = 21;
+    long variable04 = 121; // por ejemplo un numero de celular
+
+    // tipo de dato 'decimal' (f) (d)
+    float variable05 = 3.3f;
+    double variable06 = 11.1d; // o 11.11 en lugar de 11.1f
+
+    // tipo de dato 'booleano' V o F
+    boolean variable07 = false;
+    boolean variable08 = true;
+
+    // tipo de dato 'String'
+    char variable09 = 'a';
+    String variable10 = "Andreina Prati";
+
+    }
+// 9 de julio 2022
